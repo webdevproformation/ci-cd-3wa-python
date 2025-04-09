2e57c8f8a53beecd1fa083853427d87a6b100463
Malik H <malik.h@webdevpro.net>
2025-04-09 22:42:36 +0200

    port 5050

--- index.py
+++ index.py
@@ -7,4 +7,4 @@ def main():
   return "You are calling me from " + request.remote_addr + "\n"
 
 if __name__ == "__main__":
-  sample.run(host="0.0.0.0", port=8080)
+  sample.run(host="0.0.0.0", port=5050)
